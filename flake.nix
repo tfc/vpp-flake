03e0fb0f661ad0d82cb61a8f84c942c557814252
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2022-03-10 10:52:44 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/master;
+    flake-utils.url = github:numtide/flake-utils;
+
+    vpp.url = github:FDio/vpp/stable/2202;
+    vpp.flake = false;
+
+    flake-compat.url = github:edolstra/flake-compat;
+    flake-compat.flake = false;
+    flake-compat-ci.url = github:hercules-ci/flake-compat-ci;
+  };
+  outputs = { self, nixpkgs, flake-compat, flake-compat-ci, flake-utils, vpp }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.vpp = pkgs.callPackage ./vpp.nix { vppSource = vpp; };
+        defaultPackage = self.packages.${system}.vpp;
+      }
+    )) // {
+    ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
+      flake = self;
+      systems = [ "x86_64-linux" ];
+    };
+  };
+}
---
+++ vpp.nix
@@ -0,0 +1,79 @@
+{ stdenv
+, cmake
+, ninja
+, python3
+, pkg-config
+, dpdk
+, libbpf
+, libbsd
+, libelf
+, libnl
+, libuuid
+, mbedtls
+, openssl
+, rdma-core
+, check
+, subunit
+, writeScript
+, vppSource
+}:
+let
+  version = "22.02";
+  versionScript = writeScript "version" "echo ${version}-foo-bar";
+in
+
+stdenv.mkDerivation {
+  pname = "vpp";
+  inherit version;
+
+  src = "${vppSource}/src";
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    (python3.withPackages (p: [ p.ply ]))
+    pkg-config
+  ];
+
+  buildInputs = [
+    dpdk # must be at least 21.11
+    libbpf
+    libbsd
+    libelf
+    libnl
+    libuuid
+    mbedtls
+    openssl
+    rdma-core
+  ];
+
+  checkInputs = [
+    check # surprise dep: cmake lists are not `find_package`ing it
+    subunit
+  ];
+
+  doCheck = true;
+
+  postPatch = ''
+    cp ${versionScript} scripts/version
+
+    # So much about -Wall
+    sed -i \
+        -e 's/cmake pkg/cmake/' \
+        -e 's/-Wall/-Wall -Wno-stringop-overflow -Wno-unused-variable/' \
+        CMakeLists.txt
+
+    # this script produces impure results
+    sed -i \
+        -e 's/$(whoami)/itseme/' \
+        -e 's/$(hostname)/mario/' \
+        -e 's/^DATE_FMT.*$//' \
+        -e 's/^VPP_BUILD_DATE=.*$/SOURCE_DATE_EPOCH="2022-01-01T00:00:00"/' \
+        -e 's|$(git rev-parse[^)]\+)|/vpp-src|' \
+        scripts/generate_version_h
+
+    patchShebangs .
+  '';
+
+  cmakeFlags = [ "-DVPP_USE_SYSTEM_DPDK=1" ];
+}
