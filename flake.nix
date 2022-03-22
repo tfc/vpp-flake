6fd5e5dc1c2e57a1aabd9ac16b30665c66d4a9ec
Jacek Galowicz <jacek@galowicz.de>
2022-03-22 14:46:53 +0100

    Remove flake-compat as hercules 0.9 supports flakes natively

--- flake.nix
+++ flake.nix
@@ -5,12 +5,8 @@
 
     vpp.url = github:FDio/vpp/stable/2202;
     vpp.flake = false;
-
-    flake-compat.url = github:edolstra/flake-compat;
-    flake-compat.flake = false;
-    flake-compat-ci.url = github:hercules-ci/flake-compat-ci;
   };
-  outputs = { self, nixpkgs, flake-compat, flake-compat-ci, flake-utils, vpp }:
+  outputs = { self, nixpkgs, flake-utils, vpp }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -19,10 +15,5 @@
         packages.vpp = pkgs.callPackage ./vpp.nix { vppSource = vpp; };
         defaultPackage = self.packages.${system}.vpp;
       }
-    )) // {
-      ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
-        flake = self;
-        systems = [ "x86_64-linux" ];
-      };
-    };
+    ));
 }
